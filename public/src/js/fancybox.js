7d271a7937171aeca3b97caf606f6df6513f1a6f
plnttr1003 <top@nrdn.ru>
2015-07-16 17:00:38 +0300

    design

--- public/src/js/fancybox.js
+++ public/src/js/fancybox.js
@@ -29,7 +29,7 @@ function intro_runction() {
 				marginTop: 0,
 			}, 800);*/
 			$('.info_block').show()
-			$('.about_block').hide();
+			$('.about_block, .contacts_block').hide();
 			$('.header_block').addClass('black_header');
 	});
 
@@ -42,9 +42,6 @@ function intro_runction() {
 
 
 
-
-
-
 		$('.menu_item.about').click(function () {
 			var scr = $('body').height();
 			/*$('.about_block').show().css({'margin-top': - parseInt(scr) }).animate({
@@ -52,14 +49,25 @@ function intro_runction() {
 			}, 800);
 			*/
 			$('.about_block').show();
-			$('.info_block').hide();
+			$('.info_block, .contacts_block').hide();
+			$('.header_block').addClass('black_header');
+	});
+
+		$('.menu_item.contacts').click(function () {
+			var scr = $('body').height();
+			/*$('.about_block').show().css({'margin-top': - parseInt(scr) }).animate({
+				marginTop: 0,
+			}, 800);
+			*/
+			$('.contacts_block').show();
+			$('.info_block, .about_block').hide();
 			$('.header_block').addClass('black_header');
 	});
 
 	$('.goto_down_inner.about').click(function () {
 			var scr = $('body').height();
 			$('.about_block').show(100);
-			setTimeout(function() { $('.about_block').hide()}, 0);
+			setTimeout(function() { $('.about_block, .contacts_block').hide()}, 100);
 			$('.header_block').removeClass('black_header');
 		});
 
