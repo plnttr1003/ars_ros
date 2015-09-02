516a9a1765a9d78612cb65dabe7183df04805d3c
plnttr1003 <top@nrdn.ru>
2015-09-02 23:09:08 +0300

    feedback styles

--- public/src/js/lists.js
+++ public/src/js/lists.js
@@ -11,12 +11,12 @@ $(document).ready(function() {
 
 	$('.rm_user').on('click', {path:'/auth/users/remove', description: 'Удалить пользователя?'}, remove);
 	$('.rm_news').on('click', {path:'/auth/news/remove', description: 'Удалить новость?'}, remove);
-	$('.rm_event').on('click', {path:'/auth/events/remove', description: 'Удалить событие?'}, remove);
+	$('.rm_event').on('click', {path:'/auth/events/remove', description: 'Удалить альбом?'}, remove);
 	$('.rm_category').on('click', {path:'/auth/categorys/remove', description: 'Удалить категорию?'}, remove);
 	$('.rm_collect').on('click', {path:'/auth/collects/remove', description: 'Удалить коллекцию?'}, remove);
 	$('.rm_exhibit').on('click', {path:'/auth/exhibits/remove', description: 'Удалить экспонат?'}, remove);
-	$('.rm_hall').on('click', {path:'/auth/halls/remove', description: 'Удалить зал?'}, remove);
-	$('.rm_subsidiary').on('click', {path:'/auth/subsidiarys/remove', description: 'Удалить фелиал?'}, remove);
+	$('.rm_hall').on('click', {path:'/auth/halls/remove', description: 'Удалить запись?'}, remove);
+	$('.rm_subsidiary').on('click', {path:'/auth/subsidiarys/remove', description: 'Удалить страну?'}, remove);
 	$('.rm_gallery').on('click', {path:'/auth/gallerys/remove', description: 'Удалить фотографию?'}, remove);
 	$('.rm_magazine').on('click', {path:'/auth/magazines/remove', description: 'Удалить журнал?'}, remove);
 
